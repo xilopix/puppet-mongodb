00c61c0b09ad508068c2a22bf940dd979433412b
Guillaume Penaud <gpenaud@xilopix.com>
2016-02-02 16:40:40 +0100

    Tested and validated

--- lib/puppet/provider/replicaset/ruby.rb
+++ lib/puppet/provider/replicaset/ruby.rb
@@ -10,13 +10,23 @@ Puppet::Type.type(:replicaset).provide(:ruby) do
   #
   def exists?
     debug = true
-
     client_host = "#{Facter.value('fqdn')}:#{resource[:master_port]}"
+
     primary = mongo("#{client_host}", "--quiet", "--eval", "rs.isMaster().primary")
     hosts   = mongo("#{client_host}", "--quiet", "--eval", "rs.isMaster().hosts")
 
-    primary.gsub! /"/, ''
-    hosts   = hosts.split(",").map!{|x| x.tr("\n", '')}
+    if primary.empty?
+      Puppet.debug("custom-debug - No primary found - no existing replica") if debug == true
+      return false
+    elsif hosts.empty?
+      Puppet.debug("custom-debug - No hosts found - no existing replica") if debug == true
+      return false
+    else
+      primary.gsub! /"/, ''
+      hosts = hosts.split(",").map!{|x| x.tr("\n", '')}
+    end
+
+    Puppet.debug("custom-debug - Hosts: #{resource[:members]}") if debug == true
 
     if ((primary <=> client_host) && (resource[:members].sort == hosts.sort))
       Puppet.debug("custom-debug - The replicaset already exists") if debug == true
@@ -45,13 +55,31 @@ Puppet::Type.type(:replicaset).provide(:ruby) do
   def create
     debug = true
     client_host = "#{Facter.value('fqdn')}:#{resource[:master_port]}"
-    replicaset_members = resource[:members]
 
-    # initialisation of the replicaset
-    mongo("#{client_host}", "--quiet", "--eval", "rs.initiate()")
+    status = mongo("#{client_host}", "--quiet", "--eval", "rs.status().ok")
+    nb_members = mongo("#{client_host}", "--quiet", "--eval", "rs.status().members.length")
 
-    replicaset_members.each do |member|
-      mongo("#{client_host}", "--quiet", "--eval", "rs.add(\"#{member}\")")
+    replica_members = {}
+
+    # retrieve existing replica informations
+    for i in 0..(Integer(nb_members) - 1)
+      key_member = mongo("#{client_host}", "--quiet", "--eval", "rs.status().members[#{i}].name").tr("\n", '')
+      value_health = mongo("#{client_host}", "--quiet", "--eval", "rs.status().members[#{i}].health").tr("\n", '')
+
+      replica_members[key_member] = value_health
+    end
+
+    # has replica already been initialized ?
+    if Integer(status) != 1
+      Puppet.debug("custom-debug - Replica initialisation...") if debug == true
+      mongo("#{client_host}", "--quiet", "--eval", "rs.initiate()")
+    end
+
+    resource[:members].each do |member|
+      if !replica_members.has_key?(member)
+        Puppet.debug("custom-debug - Add node #{member} in the replica") if debug == true
+        mongo("#{client_host}", "--quiet", "--eval", "rs.add(\"#{member}\")")
+      end
     end
   end
 end
--- manifests/cluster/replicaset.pp
+++ manifests/cluster/replicaset.pp
@@ -9,24 +9,19 @@ define mongodb::cluster::replicaset (
   validate_numeric($replicaset_master_port)
   validate_array($replicaset_slaves)
 
-  # server set definition
-
-  $replicaset_master = "${::fqdn}:${replicaset_master_port}"
-  $replica_server_set = flatten([$replicaset_master, $replicaset_slaves])
-
   # wait for replication servers starting
 
   start_detector { "${replicaset_name}_servers_detection":
     ensure => present,
     timeout => 300,
-    servers => $replica_server_set,
+    servers => $replicaset_slaves,
     policy => all
   }
 
   replicaset { "$replicaset_name":
     ensure      => present,
     master_port => $replicaset_master_port,
-    members     => $replica_server_set,
+    members     => $replicaset_slaves,
     require     => Start_detector["${replicaset_name}_servers_detection"]
   }
 }
